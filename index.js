34058623454470c5db6164e3e38879ef9854ff63
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-22 16:10:33 -0400

    execution stack

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-console.log("test");
+// console.log("test");
+
+// function b() {
+//     console.log('Called b!');
+// }
+
+// b();
+
+// console.log(a);
+
+// var a = 'Hello World!';
+
+// console.log(a);
+
+
+
+// execution stack
 
 function b() {
-    console.log('Called b!');
+    var d;
 }
+function a() {
+    b();
+    var c;
+}
+a();
+var d;
 
-b();
+// even though a is defined below b once function a is envoked function b will then run.
+// we have global execution stack, then the a() stack, then b()stack once b is complete it will "pop" off the stack
+// returning us to complete a
 
-console.log(a);
-
-var a = 'Hello World!';
-
-console.log(a);
