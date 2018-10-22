298539a1b54bc9c0b13327eb864a254a3df85ae4
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-22 16:16:41 -0400

    variable environment

--- index.js
+++ index.js
@@ -30,3 +30,21 @@ var d;
 // we have global execution stack, then the a() stack, then b()stack once b is complete it will "pop" off the stack
 // returning us to complete a
 
+
+// functions, context, and variable environment
+// variable environment (where they are and how they relate to each other in memory)
+function b() {
+    var myVar;
+    console.log(myVar);
+}
+function a() {
+    var myVar = 2;
+    console.log(myVar);
+    b();
+}
+var myVar = 1;
+console.log(myVar);
+a();
+console.log(myVar);
+// what will myVar be at any given point?
+// 1, 2, undefined
