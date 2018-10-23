2064511a03a430bd150857e834d907400c23e02e
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-22 21:17:52 -0400

    fixed an error

--- index.js
+++ index.js
@@ -95,9 +95,8 @@ if (a > b) {
 // long running function
 function waitThreeSeconds() {
     var ms = 3000 + new Date().getTime();
-    while (new Date() < ms){
+    while (new Date() < ms){}
         console.log('finished function');
-    }
 }
 function clickHandler() {
     console.log('click event!');
